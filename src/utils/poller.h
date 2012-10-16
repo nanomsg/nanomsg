a5a8a2806e385a10a054e3daf13f0c3a38189135
Martin Sustrik <sustrik@250bpm.com>
2012-10-16 09:39:33 +0200

    Poller context made global
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- src/utils/poller.h
+++ src/utils/poller.h
@@ -32,104 +32,38 @@
     polling mechanisms that signal I/O operation completaion (IOCP). */
 
 struct sp_poller;
-struct sp_poller_hndl;
 
 struct sp_poller_vfptr {
-    void (*in) (struct sp_poller *self, struct sp_poller_hndl *hndl);
-    void (*out) (struct sp_poller *self, struct sp_poller_hndl *hndl);
-    void (*err) (struct sp_poller *self, struct sp_poller_hndl *hndl);
+    void (*in) (struct sp_poller *self);
+    void (*out) (struct sp_poller *self);
+    void (*err) (struct sp_poller *self);
 };
 
-void sp_poller_init (struct sp_poller *self, struct sp_poller_vfptr *vfptr);
+void sp_poller_ctx_init (void);
+void sp_poller_ctx_term (void);
+
+void sp_poller_init (struct sp_poller *self,
+    struct sp_poller_vfptr *vfptr, int fd);
 void sp_poller_term (struct sp_poller *self);
-void sp_poller_add_fd (struct sp_poller *self, int fd,
-    struct sp_poller_hndl *hndl);
-void sp_poller_rm_fd (struct sp_poller *self, struct sp_poller_hndl *hndl);
-void sp_poller_set_in (struct sp_poller *self, struct sp_poller_hndl *hndl);
-void sp_poller_reset_in (struct sp_poller *self, struct sp_poller_hndl *hndl);
-void sp_poller_set_out (struct sp_poller *self, struct sp_poller_hndl *hndl);
-void sp_poller_reset_out (struct sp_poller *self, struct sp_poller_hndl *hndl);
+
+void sp_poller_set_in (struct sp_poller *self);
+void sp_poller_reset_in (struct sp_poller *self);
+void sp_poller_set_out (struct sp_poller *self);
+void sp_poller_reset_out (struct sp_poller *self);
 
 #if defined SP_USE_POLL
 
-#include "thread.h"
-#include "signaler.h"
-
-#include <poll.h>
-
-struct sp_poller_hndl {
-    int index;
-};
-
 struct sp_poller {
-
-    /*  Callback functions. */
     struct sp_poller_vfptr *vfptr;
-
-    /*  Actual number of elements in the pollset. */
-    int size;
-
-    /*  Number of allocated elements in the pollset. */
-    int capacity;
-
-    /*  The pollset. */
-    struct pollfd *pollset;
-
-    /*  List of handles associated with elements in the pollset. Either points
-        to the handle associated with the file descriptor (hndl) or is part
-        of the list of removed pollitems (removed). */
-    struct sp_hndls_item {
-        struct sp_poller_hndl *hndl;
-        int prev;
-        int next;
-    } *hndls;
-
-    /*  List of removed pollitems, linked by indices. -1 means empty list. */
-    int removed;
-
-    /*  Used to send signals to the worker thread. */
-    struct sp_signaler signaler;
-
-    /*  The worker thread. */
-    struct sp_thread worker;
+    int index;
 };
 
 #elif defined SP_USE_EPOLL
 
-#include "thread.h"
-#include "signaler.h"
-
-#include <sys/epoll.h>
-
-#define SP_POLLER_MAX_EVENTS 32
-
-struct sp_poller_hndl {
+struct sp_poller {
+    struct sp_poller_vfptr *vfptr;
     int fd;
     uint32_t events;
-};
-
-struct sp_poller {
-
-    /*  Callback functions. */
-    struct sp_poller_vfptr *vfptr;
-
-    /*  Current pollset. */
-    int ep;
-
-    /*  Number of events being processed at the moment. */
-    int nevents;
-
-    /*  Index of the event being processed at the moment. */
-    int index;
-
-    /*  Events beign processed at the moment. */
-    struct epoll_event events [SP_POLLER_MAX_EVENTS];
-
-    /*  Used to send signals to the worker thread. */
-    struct sp_signaler signaler;
-
-    /*  The worker thread. */
-    struct sp_thread worker;
 };
 
 #else

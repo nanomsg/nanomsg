0f6dd291a29fd99765469485ddea93c0c2b927d6
Bruce Mitchener <bruce.mitchener@gmail.com>
2012-12-06 10:50:51 +0100

    Use offsetof() macro instead of expanded form.
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- src/utils/cont.h
+++ src/utils/cont.h
@@ -23,9 +23,11 @@
 #ifndef SP_CONT_INCLUDED
 #define SP_CONT_INCLUDED
 
+#include <stddef.h>
+
 /*  Takes a pointer to a member variable and computes pointer to the structure
     that contains it. 'type' is type of the structure, not the member. */
 #define sp_cont(ptr, type, member) \
-    ((type*) (((char*) ptr) - ((size_t) &(((type*) 0)->member))))
+    ((type*) (((char*) ptr) - offsetof(type, member)))
 
 #endif

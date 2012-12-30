028e3de33f59a15b308c6792669bb5ab980a7927
Martin Sustrik <sustrik@250bpm.com>
2012-12-30 22:30:49 +0100

    nanomsg version number displayed in man pages
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- doc/genconf.c
+++ doc/genconf.c
@@ -24,14 +24,18 @@
 #include "../src/utils/err.c"
 
 #include <stdio.h>
+#include <string.h>
 
-/*  This program generates asciidoc.conf file, filling in actual nanomsg
-    version numbers. */
+/*  This program generates asciidoc configuration file, filling in actual
+    nanomsg version numbers. */
+
+#define STR_EXPAND(x) #x
+#define STR(x) STR_EXPAND(x)
 
 int main ()
 {
     int rc;
-    size_t sz;
+    size_t sz1, sz2;
     FILE *f;
 
     const char *conf =
@@ -52,12 +56,46 @@ int main ()
         "ifdef::backend-xhtml11[]\n"
         "[linknanomsg-inlinemacro]\n"
         "<a href=\"{target}.{0}.html\">{target}{0?({0})}</a>\n"
+        "endif::backend-xhtml11[]\n"
+        "\n"
+        "ifdef::doctype-manpage[]\n"
+        "ifdef::backend-docbook[]\n"
+        "[header]\n"
+        "template::[header-declarations]\n"
+        "<refentry>\n"
+        "<refmeta>\n"
+        "<refentrytitle>{mantitle}</refentrytitle>\n"
+        "<manvolnum>{manvolnum}</manvolnum>\n"
+        "<refmiscinfo class=\"source\">nanomsg</refmiscinfo>\n"
+        "<refmiscinfo class=\"version\">" STR(SP_VERSION_MAJOR) "." STR(SP_VERSION_MINOR) "." STR(SP_VERSION_PATCH) "</refmiscinfo>\n"
+        "<refmiscinfo class=\"manual\">nanomsg manual</refmiscinfo>\n"
+        "</refmeta>\n"
+        "<refnamediv>\n"
+        "  <refname>{manname}</refname>\n"
+        "  <refpurpose>{manpurpose}</refpurpose>\n"
+        "</refnamediv>\n"
+        "endif::backend-docbook[]\n"
+        "endif::doctype-manpage[]\n"
+        "\n"
+        "ifdef::backend-xhtml11[]\n"
+        "[footer]\n"
+        "</div>\n"
+        "{disable-javascript%<div id=\"footnotes\"><hr /></div>}\n"
+        "<div id=\"footer\">\n"
+        "<div id=\"footer-text\">\n"
+        "nanomsg " STR(SP_VERSION_MAJOR) "." STR(SP_VERSION_MINOR) "." STR(SP_VERSION_PATCH) "<br />\n"
+        "Last updated {docdate} {doctime}\n"
+        "</div>\n"
+        "</div>\n"
+        "</body>\n"
+        "</html>\n"
         "endif::backend-xhtml11[]\n";
 
     f = fopen ("asciidoc.conf", "w");
     errno_assert (f);
-    sz = fwrite (conf, 1, sizeof (conf), f);
-    sp_assert (sz == sizeof (conf));
+    sz1 = strlen (conf);
+    sz2 = fwrite (conf, 1, sz1, f);
+    sp_assert (sz1 == sz2);
     rc = fclose (f);
     errno_assert (rc == 0);
 

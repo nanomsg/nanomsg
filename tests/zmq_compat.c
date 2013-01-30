048f268bf01b1147ddf92dccedf6055e53a68ffb
Martin Sustrik <sustrik@250bpm.com>
2013-01-30 10:20:19 +0100

    zmq_compat test tests native NN socket and option constants
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- tests/zmq_compat.c
+++ tests/zmq_compat.c
@@ -33,25 +33,39 @@ int main ()
     void *ctx;
     void *s1;
     void *s2;
-    uint64_t sndbuf;
+    uint64_t zmq_sndbuf;
+    int nn_sndbuf;
     zmq_msg_t msg1;
     zmq_msg_t msg2;
 
     ctx = zmq_init (1);
     errno_assert (ctx);
 
+    /*  Open a PAIR socket using ZMQ-specific name. */
     s1 = zmq_socket (ctx, ZMQ_PAIR);
     errno_assert (s1);
-    s2 = zmq_socket (ctx, ZMQ_PAIR);
+
+    /*  Open a PAIR socket using NN-specific constant. */
+    s2 = zmq_socket (ctx, 1016);
     errno_assert (s2);
+
+    /*  Check wiring up the topology. */
     rc = zmq_bind (s1, "inproc://a");
     errno_assert (rc == 0);
     rc = zmq_connect (s2, "inproc://a");
     errno_assert (rc == 0);
-    sndbuf = 128 * 1024;
-    rc = zmq_setsockopt (s2, ZMQ_SNDBUF, &sndbuf, sizeof (sndbuf));
+
+    /*  Set an option using ZMQ-specific name. */
+    zmq_sndbuf = 128 * 1024;
+    rc = zmq_setsockopt (s1, ZMQ_SNDBUF, &zmq_sndbuf, sizeof (zmq_sndbuf));
     errno_assert (rc == 0);
 
+    /*  Set an option using NN-specific constant. */
+    nn_sndbuf = 128 * 1024;
+    rc = zmq_setsockopt (s2, 1002, &nn_sndbuf, sizeof (nn_sndbuf));
+    errno_assert (rc == 0);
+
+    /*  Send a message. */
     rc = zmq_msg_init_size (&msg1, 3);
     errno_assert (rc == 0);
     memcpy (zmq_msg_data (&msg1), "ABC", 3);
@@ -60,6 +74,7 @@ int main ()
     rc = zmq_msg_close (&msg1);
     errno_assert (rc == 0);
 
+    /*  Receive a message. */
     rc = zmq_msg_init (&msg2);
     errno_assert (rc == 0);
     rc = zmq_recv (s1, &msg2, 0);
@@ -68,6 +83,7 @@ int main ()
     rc = zmq_msg_close (&msg2);
     errno_assert (rc == 0);
 
+    /*  Clean up. */
     rc = zmq_close (s2);
     errno_assert (rc == 0);
     rc = zmq_close (s1);

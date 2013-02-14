d3ca80010da81631bf5cbb576d8ac4604f2b93ce
Martin Sustrik <sustrik@250bpm.com>
2013-02-14 08:07:08 +0100

    Survey test checks whether stale responses are dropped
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- tests/survey.c
+++ tests/survey.c
@@ -31,11 +31,11 @@ int main ()
     int surveyor;
     int respondent1;
     int respondent2;
+    int respondent3;
     int deadline;
     char buf [7];
 
-    /*  Test a simple survey with two respondents. */
-
+    /*  Test a simple survey with three respondents. */
     surveyor = nn_socket (AF_SP, NN_SURVEYOR);
     errno_assert (surveyor != -1);
     deadline = 500;
@@ -52,11 +52,17 @@ int main ()
     errno_assert (respondent2 != -1);
     rc = nn_connect (respondent2, "inproc://a");
     errno_assert (rc >= 0);
+    respondent3 = nn_socket (AF_SP, NN_RESPONDENT);
+    errno_assert (respondent3 != -1);
+    rc = nn_connect (respondent3, "inproc://a");
+    errno_assert (rc >= 0);
 
+    /*  Send the survey. */
     rc = nn_send (surveyor, "ABC", 3, 0);
     errno_assert (rc >= 0);
     nn_assert (rc == 3);
 
+    /*  First respondent answers. */
     rc = nn_recv (respondent1, buf, sizeof (buf), 0);
     errno_assert (rc >= 0);
     nn_assert (rc == 3);
@@ -64,6 +70,7 @@ int main ()
     errno_assert (rc >= 0);
     nn_assert (rc == 3);
 
+    /*  Second respondent answers. */
     rc = nn_recv (respondent2, buf, sizeof (buf), 0);
     errno_assert (rc >= 0);
     nn_assert (rc == 3);
@@ -71,6 +78,7 @@ int main ()
     errno_assert (rc >= 0);
     nn_assert (rc == 3);
 
+    /*  Surveyor gets the responses. */
     rc = nn_recv (surveyor, buf, sizeof (buf), 0);
     errno_assert (rc >= 0);
     nn_assert (rc == 3);
@@ -78,6 +86,24 @@ int main ()
     errno_assert (rc >= 0);
     nn_assert (rc == 3);
 
+    /*  There are no more responses. Surveyor hits the deadline. */
+    rc = nn_recv (surveyor, buf, sizeof (buf), 0);
+    errno_assert (rc == -1 && nn_errno () == EFSM);
+
+    /*  Third respondent answers (it have already missed the deadline). */
+    rc = nn_recv (respondent3, buf, sizeof (buf), 0);
+    errno_assert (rc >= 0);
+    nn_assert (rc == 3);
+    rc = nn_send (respondent3, "GHI", 3, 0);
+    errno_assert (rc >= 0);
+    nn_assert (rc == 3);
+
+    /*  Surveyor initiates new survey. */
+    rc = nn_send (surveyor, "ABC", 3, 0);
+    errno_assert (rc >= 0);
+    nn_assert (rc == 3);
+
+    /* Check that stale response from third respondent is not delivered. */
     rc = nn_recv (surveyor, buf, sizeof (buf), 0);
     errno_assert (rc == -1 && nn_errno () == EFSM);
 
@@ -86,6 +112,8 @@ int main ()
     rc = nn_close (respondent1);
     errno_assert (rc == 0);    
     rc = nn_close (respondent2);
+    errno_assert (rc == 0);
+    rc = nn_close (respondent3);
     errno_assert (rc == 0);
 
     return 0;

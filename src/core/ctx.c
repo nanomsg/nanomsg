f7d708f315a27c4eb1e0a51baa98230688c5d711
Martin Sustrik <sustrik@250bpm.com>
2013-02-15 04:14:35 +0100

    nn_device polls for POLLERR
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- src/core/ctx.c
+++ src/core/ctx.c
@@ -811,16 +811,17 @@ int nn_device (int s1, int s2)
     int rc;
     int s1in;
     int s1out;
+    int s1err;
     int s2in;
     int s2out;
+    int s2err;
     size_t optsz;
-    struct pollfd pfd [4];
+    struct pollfd pfd [6];
     void *msg;
 
     /*  Get file descriptors to poll on. */
     /*  TODO: To avoid using too much resource, some internal polling mechanism
         should be used here instead of file descriptors. */
-    /*  TODO: What about errors (ETERM)? How should we poll for errors? */
     optsz = sizeof (s1in);
     rc = nn_getsockopt (s1, NN_SOL_SOCKET, NN_RCVFD, &s1in, &optsz);
     if (nn_slow (rc != 0))
@@ -831,6 +832,11 @@ int nn_device (int s1, int s2)
     if (nn_slow (rc != 0))
         return -1;
     nn_assert (optsz == sizeof (s1out));
+    optsz = sizeof (s1err);
+    rc = nn_getsockopt (s1, NN_SOL_SOCKET, NN_ERRFD, &s1err, &optsz);
+    if (nn_slow (rc != 0))
+        return -1;
+    nn_assert (optsz == sizeof (s1err));
     optsz = sizeof (s2in);
     rc = nn_getsockopt (s2, NN_SOL_SOCKET, NN_RCVFD, &s2in, &optsz);
     if (nn_slow (rc != 0))
@@ -841,53 +847,68 @@ int nn_device (int s1, int s2)
     if (nn_slow (rc != 0))
         return -1;
     nn_assert (optsz == sizeof (s2out));
+    optsz = sizeof (s2err);
+    rc = nn_getsockopt (s1, NN_SOL_SOCKET, NN_ERRFD, &s2err, &optsz);
+    if (nn_slow (rc != 0))
+        return -1;
+    nn_assert (optsz == sizeof (s2err));
 
     /*  Initialise the pollset. */
     pfd [0].fd = s1in;
     pfd [0].events = POLLIN;
     pfd [1].fd = s1out;
     pfd [1].events = POLLIN;
-    pfd [2].fd = s2in;
+    pfd [2].fd = s1err;
     pfd [2].events = POLLIN;
-    pfd [3].fd = s2out;
+    pfd [3].fd = s2in;
     pfd [3].events = POLLIN;
+    pfd [4].fd = s2out;
+    pfd [4].events = POLLIN;
+    pfd [5].fd = s2err;
+    pfd [5].events = POLLIN;
 
     while (1) {
 
         /*  Wait for network events. */
-        rc = poll (pfd, 4, -1);
+        rc = poll (pfd, 6, -1);
         errno_assert (rc >= 0);
         if (nn_slow (rc < 0 && errno == EINTR))
             return -1;
         nn_assert (rc != 0);
+
+        /*  Process the eventual errors. */
+        if (nn_slow (pfd [2].revents & POLLIN || pfd [5].revents & POLLIN)) {
+            errno = ETERM;
+            return -1;
+        }
 
         /*  Process the events. */
         if (pfd [0].revents & POLLIN)
             pfd [0].events = 0;
         if (pfd [1].revents & POLLIN)
             pfd [1].events = 0;
-        if (pfd [2].revents & POLLIN)
-            pfd [2].events = 0;
         if (pfd [3].revents & POLLIN)
             pfd [3].events = 0;
+        if (pfd [4].revents & POLLIN)
+            pfd [4].events = 0;
 
         /*  If possible, pass the message from s1 to s2. */
-        if (pfd [0].events == 0 && pfd [3].events == 0) {
+        if (pfd [0].events == 0 && pfd [4].events == 0) {
             rc = nn_recv (pfd [0].fd, &msg, NN_MSG, NN_DONTWAIT);
             errno_assert (rc >= 0);
-            rc = nn_send (pfd [3].fd, &msg, NN_MSG, NN_DONTWAIT);
+            rc = nn_send (pfd [4].fd, &msg, NN_MSG, NN_DONTWAIT);
             errno_assert (rc >= 0);
             pfd [0].events = POLLIN;
-            pfd [3].events = POLLIN;
+            pfd [4].events = POLLIN;
         }
 
         /*  If possible, pass the message from s2 to s1. */
-        if (pfd [2].events == 0 && pfd [1].events == 0) {
-            rc = nn_recv (pfd [2].fd, &msg, NN_MSG, NN_DONTWAIT);
+        if (pfd [3].events == 0 && pfd [1].events == 0) {
+            rc = nn_recv (pfd [3].fd, &msg, NN_MSG, NN_DONTWAIT);
             errno_assert (rc >= 0);
             rc = nn_send (pfd [1].fd, &msg, NN_MSG, NN_DONTWAIT);
             errno_assert (rc >= 0);
-            pfd [2].events = POLLIN;
+            pfd [3].events = POLLIN;
             pfd [1].events = POLLIN;
         }
     }

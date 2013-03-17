87c73523fcbd141debeb6960673c5600c797bffa
Evan Wies <evan@neomantra.net>
2013-03-17 08:30:38 +0100

    nn_symbol queries the names and values of nanomsg symbols.
    /*  Returns the symbol name (e.g. "NN_REQ") and value at a specified index.   */
    /*  If the index is out-of-range, returns NULL and sets errno to EINVAL       */
    /*  General usage is to start at i=0 and iterate until NULL is returned.      */
    NN_EXPORT const char *nn_symbol (int i, int* value);
    
    With the addition of nn_symbol to dynamically query nanomsg symbols (constants),
    the nn_version function is obsolete.  C programs can get the version information
    directly from the #defines and other languages can extract it from the various
    NN_VERSION_* symbols.
    
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

---
+++ tests/symbol.c
@@ -0,0 +1,48 @@
+/*
+    Copyright (c) 2013 Evan Wies <evan@neomantra.net>
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"),
+    to deal in the Software without restriction, including without limitation
+    the rights to use, copy, modify, merge, publish, distribute, sublicense,
+    and/or sell copies of the Software, and to permit persons to whom
+    the Software is furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+    IN THE SOFTWARE.
+*/
+
+#include "../src/core/symbol.c"
+#include "../src/utils/err.c"
+
+int main ()
+{
+    int value, i;
+
+    nn_assert (nn_symbol (-1, NULL) == NULL);
+    nn_assert (nn_errno () == EINVAL);
+
+    nn_assert (nn_symbol (2000, NULL) == NULL);
+    nn_assert (nn_errno () == EINVAL);
+
+    nn_assert (nn_symbol (6, &value) != NULL);
+    nn_assert (value != 0);
+
+    for (i = 0; ; ++i) {
+        const char* name = nn_symbol (i, &value);
+        if (name == NULL) {
+            nn_assert (nn_errno () == EINVAL);
+            break;
+        }
+    }
+    nn_assert (i == SYM_VALUE_NAMES_LEN);
+}
+

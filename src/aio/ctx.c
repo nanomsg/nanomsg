593a6b5d35381543a42ee9be7cb91be1a53bb141
Martin Sustrik <sustrik@250bpm.com>
2013-04-17 15:06:32 +0200

    nn_ctx_task implemented
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- src/aio/ctx.c
+++ src/aio/ctx.c
@@ -23,15 +23,32 @@
 #include "ctx.h"
 
 #include "../utils/err.h"
+#include "../utils/cont.h"
+
+void nn_ctx_task_init (struct nn_ctx_task *self, struct nn_callback *callback,
+    void *source, int type)
+{
+    self->callback = callback;
+    self->source = source;
+    self->type = type;
+    nn_queue_item_init (&self->item);
+}
+
+void nn_ctx_task_term (struct nn_ctx_task *self)
+{
+    nn_queue_item_term (&self->item);
+}
 
 void nn_ctx_init (struct nn_ctx *self, struct nn_pool *pool)
 {
     nn_mutex_init (&self->sync);
     self->pool = pool;
+    nn_queue_init (&self->tasks);
 }
 
 void nn_ctx_term (struct nn_ctx *self)
 {
+    nn_queue_term (&self->tasks);
     nn_mutex_term (&self->sync);
 }
 
@@ -42,11 +59,26 @@ void nn_ctx_enter (struct nn_ctx *self)
 
 void nn_ctx_leave (struct nn_ctx *self)
 {
+    struct nn_ctx_task *task;
+
+    /*  Process any queued tasks before leaving the AIO context. */
+    while (1) {
+        task = nn_cont (nn_queue_pop (&self->tasks), struct nn_ctx_task, item);
+        if (!task)
+            break;
+        task->callback->fn (task->callback, task->source, task->type);
+    }
+
     nn_mutex_unlock (&self->sync);
 }
 
 struct nn_worker *nn_ctx_choose_worker (struct nn_ctx *self)
 {
-    nn_assert (0);
+    return nn_pool_choose_worker (self->pool);
+}
+
+void nn_ctx_execute (struct nn_ctx *self, struct nn_ctx_task *task)
+{
+    nn_queue_push (&self->tasks, &task->item);
 }
 
--- src/aio/ctx.h
+++ src/aio/ctx.h
@@ -24,15 +24,29 @@
 #define NN_CTX_INCLUDED
 
 #include "../utils/mutex.h"
+#include "../utils/queue.h"
 
 #include "worker.h"
+#include "callback.h"
 #include "pool.h"
 
 /*  AIO context for objects using AIO subsystem. */
 
+struct nn_ctx_task {
+    struct nn_callback *callback;
+    void *source;
+    int type;
+    struct nn_queue_item item;
+};
+
+void nn_ctx_task_init (struct nn_ctx_task *self, struct nn_callback *callback,
+    void *source, int type);
+void nn_ctx_task_term (struct nn_ctx_task *self);
+
 struct nn_ctx {
     struct nn_mutex sync;
     struct nn_pool *pool;
+    struct nn_queue tasks;
 };
 
 void nn_ctx_init (struct nn_ctx *self, struct nn_pool *pool);
@@ -42,6 +56,8 @@ void nn_ctx_enter (struct nn_ctx *self);
 void nn_ctx_leave (struct nn_ctx *self);
 
 struct nn_worker *nn_ctx_choose_worker (struct nn_ctx *self);
+
+void nn_ctx_execute (struct nn_ctx *self, struct nn_ctx_task *task);
 
 #endif
 

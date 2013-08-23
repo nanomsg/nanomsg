9748d06913ce51dabc6a0cef6cdd9eaff05688c4
Martin Sustrik <sustrik@250bpm.com>
2013-08-23 18:51:00 +0200

    bashisms remvoed from package_version.sh
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- package_version.sh
+++ package_version.sh
@@ -21,15 +21,15 @@
 
 if [ -d .git ]; then
     #  Retrieve the version from the last git tag.
-    VER=$(git describe --always | sed -e "s:v::")
-    if [ x"$(git diff-index --name-only HEAD)" != x ]; then
+    VER=`git describe --always | sed -e "s:v::"`
+    if [ x"`git diff-index --name-only HEAD`" != x ]; then
         #  If the sources have been changed locally, add -dirty to the version.
         VER="${VER}-dirty"
     fi
 elif [ -f .version ]; then
     #  If git is not available (e.g. when building from source package)
     #  we can extract the package version from .version file.
-    VER=$(< .version)
+    VER=`< .version`
 else
     #  The package version cannot be retrieved.
     VER="Unknown"

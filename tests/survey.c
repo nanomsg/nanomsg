ee0e2c05f22ec7213e49ad049480612cb5125f99
Paul Colomiets <paul@colomiets.name>
2013-09-17 17:43:00 +0300

    Refactored `survey` test

--- tests/survey.c
+++ tests/survey.c
@@ -23,8 +23,7 @@
 #include "../src/nn.h"
 #include "../src/survey.h"
 
-#include "../src/utils/err.c"
-#include "../src/utils/sleep.c"
+#include "testutil.h"
 
 #define SOCKET_ADDRESS "inproc://test"
 
@@ -39,85 +38,53 @@ int main ()
     char buf [7];
 
     /*  Test a simple survey with three respondents. */
-    surveyor = nn_socket (AF_SP, NN_SURVEYOR);
-    errno_assert (surveyor != -1);
+    surveyor = test_socket (AF_SP, NN_SURVEYOR);
     deadline = 500;
     rc = nn_setsockopt (surveyor, NN_SURVEYOR, NN_SURVEYOR_DEADLINE,
         &deadline, sizeof (deadline));
     errno_assert (rc == 0);
-    rc = nn_bind (surveyor, SOCKET_ADDRESS);
-    errno_assert (rc >= 0);
-    respondent1 = nn_socket (AF_SP, NN_RESPONDENT);
-    errno_assert (respondent1 != -1);
-    rc = nn_connect (respondent1, SOCKET_ADDRESS);
-    errno_assert (rc >= 0);
-    respondent2 = nn_socket (AF_SP, NN_RESPONDENT);
-    errno_assert (respondent2 != -1);
-    rc = nn_connect (respondent2, SOCKET_ADDRESS);
-    errno_assert (rc >= 0);
-    respondent3 = nn_socket (AF_SP, NN_RESPONDENT);
-    errno_assert (respondent3 != -1);
-    rc = nn_connect (respondent3, SOCKET_ADDRESS);
-    errno_assert (rc >= 0);
+    test_bind (surveyor, SOCKET_ADDRESS);
+    respondent1 = test_socket (AF_SP, NN_RESPONDENT);
+    test_connect (respondent1, SOCKET_ADDRESS);
+    respondent2 = test_socket (AF_SP, NN_RESPONDENT);
+    test_connect (respondent2, SOCKET_ADDRESS);
+    respondent3 = test_socket (AF_SP, NN_RESPONDENT);
+    test_connect (respondent3, SOCKET_ADDRESS);
 
     /*  Send the survey. */
-    rc = nn_send (surveyor, "ABC", 3, 0);
-    errno_assert (rc >= 0);
-    nn_assert (rc == 3);
+    test_send (surveyor, "ABC");
 
     /*  First respondent answers. */
-    rc = nn_recv (respondent1, buf, sizeof (buf), 0);
-    errno_assert (rc >= 0);
-    nn_assert (rc == 3);
-    rc = nn_send (respondent1, "DEF", 3, 0);
-    errno_assert (rc >= 0);
-    nn_assert (rc == 3);
+    test_recv (respondent1, "ABC");
+    test_send (respondent1, "DEF");
 
     /*  Second respondent answers. */
-    rc = nn_recv (respondent2, buf, sizeof (buf), 0);
-    errno_assert (rc >= 0);
-    nn_assert (rc == 3);
-    rc = nn_send (respondent2, "DEF", 3, 0);
-    errno_assert (rc >= 0);
-    nn_assert (rc == 3);
+    test_recv (respondent2, "ABC");
+    test_send (respondent2, "DEF");
 
     /*  Surveyor gets the responses. */
-    rc = nn_recv (surveyor, buf, sizeof (buf), 0);
-    errno_assert (rc >= 0);
-    nn_assert (rc == 3);
-    rc = nn_recv (surveyor, buf, sizeof (buf), 0);
-    errno_assert (rc >= 0);
-    nn_assert (rc == 3);
+    test_recv (surveyor, "DEF");
+    test_recv (surveyor, "DEF");
 
     /*  There are no more responses. Surveyor hits the deadline. */
     rc = nn_recv (surveyor, buf, sizeof (buf), 0);
     errno_assert (rc == -1 && nn_errno () == EFSM);
 
     /*  Third respondent answers (it have already missed the deadline). */
-    rc = nn_recv (respondent3, buf, sizeof (buf), 0);
-    errno_assert (rc >= 0);
-    nn_assert (rc == 3);
-    rc = nn_send (respondent3, "GHI", 3, 0);
-    errno_assert (rc >= 0);
-    nn_assert (rc == 3);
+    test_recv (respondent3, "ABC");
+    test_send (respondent3, "GHI");
 
     /*  Surveyor initiates new survey. */
-    rc = nn_send (surveyor, "ABC", 3, 0);
-    errno_assert (rc >= 0);
-    nn_assert (rc == 3);
+    test_send (surveyor, "ABC");
 
     /*  Check that stale response from third respondent is not delivered. */
     rc = nn_recv (surveyor, buf, sizeof (buf), 0);
     errno_assert (rc == -1 && nn_errno () == EFSM);
 
-    rc = nn_close (surveyor);
-    errno_assert (rc == 0);
-    rc = nn_close (respondent1);
-    errno_assert (rc == 0);    
-    rc = nn_close (respondent2);
-    errno_assert (rc == 0);
-    rc = nn_close (respondent3);
-    errno_assert (rc == 0);
+    test_close (surveyor);
+    test_close (respondent1);
+    test_close (respondent2);
+    test_close (respondent3);
 
     return 0;
 }

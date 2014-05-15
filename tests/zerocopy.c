57694435126d77d5483ddf9ae9bc99870008881c
Achille Roussel <ach@fb.com>
2014-05-15 16:41:34 -0700

    add copyright notice

--- tests/zerocopy.c
+++ tests/zerocopy.c
@@ -1,5 +1,6 @@
 /*
     Copyright (c) 2013 GoPivotal, Inc.  All rights reserved.
+    Copyright (c) 2014 Achille Roussel. All rights reserved.
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"),

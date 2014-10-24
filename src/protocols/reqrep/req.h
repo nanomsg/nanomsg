9c7c4e7644d155fc7913c3dec446813cf5b61b81
Drew Crawford <drew@sealedabstract.com>
2014-10-24 12:32:57 +0200

    Improving the extensibility for req/rep
    This is an improvement on nanomsg#264, marking static members as non-static so they can be referenced outside req.c and rep.c
    
    In addition, methods nn_req_csend and nn_req_crecv seem to have been added, that perform the old functions of nn_req_send and nn_req_recv respectively.  Exposing these methods for extensibility.
    
    Signed-off-by: Drew Crawford <drew@sealedabstract.com>
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- src/protocols/reqrep/req.h
+++ src/protocols/reqrep/req.h
@@ -52,28 +52,30 @@ extern struct nn_socktype *nn_req_socktype;
 
 /*  Some users may want to extend the REQ protocol similar to how REQ extends XREQ.
     Expose these methods to improve extensibility. */
-static void nn_req_init (struct nn_req *self,
+void nn_req_init (struct nn_req *self,
     const struct nn_sockbase_vfptr *vfptr, void *hint);
-static void nn_req_term (struct nn_req *self);
-static int nn_req_inprogress (struct nn_req *self);
-static void nn_req_handler (struct nn_fsm *self, int src, int type,
+void nn_req_term (struct nn_req *self);
+int nn_req_inprogress (struct nn_req *self);
+void nn_req_handler (struct nn_fsm *self, int src, int type,
     void *srcptr);
-static void nn_req_shutdown (struct nn_fsm *self, int src, int type,
+void nn_req_shutdown (struct nn_fsm *self, int src, int type,
     void *srcptr);
-static void nn_req_action_send (struct nn_req *self, int allow_delay);
+void nn_req_action_send (struct nn_req *self, int allow_delay);
 
 /*  Implementation of nn_sockbase's virtual functions. */
-static void nn_req_stop (struct nn_sockbase *self);
-static void nn_req_destroy (struct nn_sockbase *self);
-static void nn_req_in (struct nn_sockbase *self, struct nn_pipe *pipe);
-static void nn_req_out (struct nn_sockbase *self, struct nn_pipe *pipe);
-static int nn_req_events (struct nn_sockbase *self);
-static int nn_req_csend (struct nn_sockbase *self, struct nn_msg *msg);
-static void nn_req_rm (struct nn_sockbase *self, struct nn_pipe *pipe);
-static int nn_req_crecv (struct nn_sockbase *self, struct nn_msg *msg);
-static int nn_req_setopt (struct nn_sockbase *self, int level, int option,
+void nn_req_stop (struct nn_sockbase *self);
+void nn_req_destroy (struct nn_sockbase *self);
+void nn_req_in (struct nn_sockbase *self, struct nn_pipe *pipe);
+void nn_req_out (struct nn_sockbase *self, struct nn_pipe *pipe);
+int nn_req_events (struct nn_sockbase *self);
+int nn_req_csend (struct nn_sockbase *self, struct nn_msg *msg);
+void nn_req_rm (struct nn_sockbase *self, struct nn_pipe *pipe);
+int nn_req_crecv (struct nn_sockbase *self, struct nn_msg *msg);
+int nn_req_setopt (struct nn_sockbase *self, int level, int option,
     const void *optval, size_t optvallen);
-static int nn_req_getopt (struct nn_sockbase *self, int level, int option,
+int nn_req_getopt (struct nn_sockbase *self, int level, int option,
     void *optval, size_t *optvallen);
+int nn_req_csend (struct nn_sockbase *self, struct nn_msg *msg);
+int nn_req_crecv (struct nn_sockbase *self, struct nn_msg *msg);
 
 #endif

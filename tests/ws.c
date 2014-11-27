fdabf7f7e3d0ac2f75a11f0af05560efc4f9c97b
Martin Sustrik <sustrik@250bpm.com>
2014-11-27 21:54:33 +0100

    ws tests improved
    Signed-off-by: Martin Sustrik <sustrik@250bpm.com>

--- tests/ws.c
+++ tests/ws.c
@@ -1,6 +1,6 @@
 /*
-    Copyright (c) 2012 250bpm s.r.o.  All rights reserved.
     Copyright (c) 2014 Wirebird Labs LLC.  All rights reserved.
+    Copyright (c) 2014 250bpm s.r.o.  All rights reserved.
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"),
@@ -36,8 +36,7 @@ int main ()
     int rc;
     int sb;
     int sc;
-    //int opt;
-    //size_t sz;
+    int i;
 
     /*  Try closing bound but unconnected socket. */
     sb = test_socket (AF_SP, NN_PAIR);
@@ -52,23 +51,6 @@ int main ()
 
     /*  Open the socket anew. */
     sc = test_socket (AF_SP, NN_PAIR);
-
-    /*  Check socket options. */
-    //sz = sizeof (opt);
-    //rc = nn_getsockopt (sc, NN_WS, NN_WS_HANDSHAKE_TIMEOUT, &opt, &sz);
-    //errno_assert (rc == 0);
-    //nn_assert (sz == sizeof (opt));
-    //nn_assert (opt == 1000);
-    //opt = 100;
-    //sz = sizeof (opt);
-    //rc = nn_getsockopt (sc, NN_WS, NN_WS_HANDSHAKE_TIMEOUT, &opt, &sz);
-    //errno_assert (rc == 0);
-    //nn_assert (sz == sizeof (opt));
-    //nn_assert (opt == 100);
-
-    /*  Default port 80 should be assumed if not explicitly declared. */
-    rc = nn_connect (sc, "ws://127.0.0.1");
-    errno_assert (rc >= 0);
 
     /*  Try using invalid address strings. */
     rc = nn_connect (sc, "ws://*:");
@@ -117,5 +99,36 @@ int main ()
 
     test_close (sc);
 
+    sb = test_socket (AF_SP, NN_PAIR);
+    test_bind (sb, "ws://127.0.0.1:5555");
+    sc = test_socket (AF_SP, NN_PAIR);
+    test_connect (sc, "ws://127.0.0.1:5555");
+
+    /*  Ping-pong test. */
+    for (i = 0; i != 100; ++i) {
+        test_send (sc, "ABC");
+        test_recv (sb, "ABC");
+        test_send (sb, "DEF");
+        test_recv (sc, "DEF");
+    }
+
+    /*  Batch transfer test. */
+    for (i = 0; i != 100; ++i) {
+        test_send (sc, "01234567890123456789012345678901234567890123456789"
+            "01234567890123456789012345678901234567890123456789"
+            "01234567890123456789012345678901234567890123456789"
+            "01234567890123456789012345678901234567890123456789");
+    }
+    for (i = 0; i != 100; ++i) {
+        test_recv (sb, "01234567890123456789012345678901234567890123456789"
+            "01234567890123456789012345678901234567890123456789"
+            "01234567890123456789012345678901234567890123456789"
+            "01234567890123456789012345678901234567890123456789");
+    }
+
+    test_close (sc);
+    test_close (sb);
+
     return 0;
 }
+

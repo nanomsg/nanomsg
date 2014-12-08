468949edbe3a6990cc414356be36c54b52f2fcb8
Timothee Besset <ttimo@ttimo.net>
2014-12-08 20:18:24 +0000

    Merging core winxp support changes from our production branch:
    - added check for inte_pton in the DLL, if it does not exist we use our own implementation.
    - added nn_cancelioex, nn_getqueuedcompletionstatusex, nn_isvistaplus.
    - implemented checks for PIPE_REJECT_REMOTE_CLIENTS since this is not supported in XP
    - implemented nn_worker_cancelio, this will post a status that the worker thread can respond to by Canceling the handle.
    - implemented worker routine for XP using GetQueuedCompletionStatus.
    - refactored the implementation into two functions namely nn_worker_routine_xp and nn_worker_routine_vista. Tha proper routine will run depending on the running OS.

---
+++ src/aio/iocp.c
@@ -0,0 +1,60 @@
+#include "iocp.h"
+#include "../utils/err.h"
+
+#ifdef NN_HAVE_WINDOWS
+typedef BOOL	 (WINAPI *api_CancelIoEx)(HANDLE hFile, LPOVERLAPPED lpOverlapped);
+typedef BOOL	 (WINAPI *api_GetQueuedCompletionStatusEx)(HANDLE CompletionPort, LPOVERLAPPED_ENTRY lpCompletionPortEntries, ULONG ulCount, PULONG ulNumEntriesRemoved, DWORD dwMilliseconds, BOOL fAlertable);
+
+/* Holds the pointer to our > Vista+ APIs*/
+api_CancelIoEx ptr_api_CancelIoEx = NULL;
+api_GetQueuedCompletionStatusEx ptr_api_GetQueuedCompletionStatusEx = NULL;
+
+/* on Vista and up this is true*/
+BOOL bIsVistaPlus = TRUE;
+BOOL bOSQueried = FALSE;
+
+BOOL nn_cancelioex(HANDLE hFile, LPOVERLAPPED lpOverlapped)
+{
+	/* Check our if the DLLs has the normal APIs if not set our own implementation*/
+	if(ptr_api_CancelIoEx == NULL) {
+		ptr_api_CancelIoEx = (api_CancelIoEx)GetProcAddress(GetModuleHandleA("Kernel32.dll"), "CancelIoEx");
+	}
+
+	nn_assert(ptr_api_CancelIoEx); //should be calling this in Vista+
+
+	return ptr_api_CancelIoEx(hFile, lpOverlapped);
+}
+
+BOOL nn_getqueuedcompletionstatusex(HANDLE CompletionPort, LPOVERLAPPED_ENTRY lpCompletionPortEntries, ULONG ulCount, 
+	PULONG ulNumEntriesRemoved, DWORD dwMilliseconds, BOOL fAlertable)
+{
+	HMODULE hLib = NULL;
+	/* Check our if the DLLs has the normal APIs if not set our own implementation*/
+	if(ptr_api_GetQueuedCompletionStatusEx == NULL) {
+		ptr_api_GetQueuedCompletionStatusEx = (api_GetQueuedCompletionStatusEx)GetProcAddress(GetModuleHandleA("Kernel32.dll"), "GetQueuedCompletionStatusEx");
+	}
+
+	nn_assert(ptr_api_GetQueuedCompletionStatusEx); //should be calling this in Vista+
+
+	return ptr_api_GetQueuedCompletionStatusEx(CompletionPort, lpCompletionPortEntries, ulCount, ulNumEntriesRemoved, dwMilliseconds, fAlertable);
+}
+#define SIM_XP 1
+BOOL nn_isvistaplus()
+{
+#if SIM_XP
+	return FALSE;
+#else
+	OSVERSIONINFO osvi;
+	if(bOSQueried) return bIsVistaPlus;
+
+	memset(&osvi, 0, sizeof(osvi));
+	osvi.dwOSVersionInfoSize = sizeof(osvi);
+	nn_assert(GetVersionEx(&osvi));	
+	if(osvi.dwMajorVersion < 6) bIsVistaPlus = FALSE;
+	bOSQueried = TRUE;
+
+	return bIsVistaPlus;
+#endif
+}
+
+#endif

177d1214cd4dc183fa1009b6155af7c7fbc31d71
Garrett D'Amore <garrett@damore.org>
2015-02-19 09:03:54 -0800

    surveyor protocol broken - no backtrace (fixes #387)

--- src/survey.h
+++ src/survey.h
@@ -1,5 +1,6 @@
 /*
     Copyright (c) 2012 Martin Sustrik  All rights reserved.
+    Copyright 2015 Garrett D'Amore <garrett@damore.org>
 
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"),
@@ -29,8 +30,11 @@ extern "C" {
 
 #define NN_PROTO_SURVEY 6
 
-#define NN_SURVEYOR (NN_PROTO_SURVEY * 16 + 0)
-#define NN_RESPONDENT (NN_PROTO_SURVEY * 16 + 1)
+/*  NB: Version 0 used 16 + 0/1.  That version lacked backtraces, and so
+    is wire-incompatible with this version. */
+
+#define NN_SURVEYOR (NN_PROTO_SURVEY * 16 + 2)
+#define NN_RESPONDENT (NN_PROTO_SURVEY * 16 + 3)
 
 #define NN_SURVEYOR_DEADLINE 1
 

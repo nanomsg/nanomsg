02c80c654a0cb5524dba007a383da96dd4536b79
Garrett D'Amore <garrett@damore.org>
2015-11-18 12:29:41 -0800

    Only use _snprintf_s madness ifdef _MSC.

--- src/core/global.c
+++ src/core/global.c
@@ -1169,7 +1169,15 @@ static void nn_global_submit_errors (int i, struct nn_sock *s,
             ep = nn_cont (it, struct nn_ep, item);
 
             if (ep->last_errno) {
-#ifdef NN_HAVE_WINDOWS
+#ifdef _MSC_VER
+                /*
+                 * On Windows, when using Microsoft C, we need to use their
+                 * non-standard replacement for snprintf.  However, when we
+                 * use mingw, we get a non-broken standard conforming
+                 * snprintf instead, so we *only* override for MS C.  (Note
+                 * that mingw apparently *lacks* the Microsoft specific
+                 * version, for reasons that elude me.)
+                 */
                 len = _snprintf_s (curbuf, buf_left, _TRUNCATE,
                     " nanomsg: Endpoint %d [%s] error: %s\n",
                     ep->eid, nn_ep_getaddr (ep), nn_strerror (ep->last_errno));

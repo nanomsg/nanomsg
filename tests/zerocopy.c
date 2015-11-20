545e8be9cf0e024e8aa554a6fc8dec940f228fd3
Garrett D'Amore <garrett@damore.org>
2015-11-20 06:30:22 -0800

    fixes #533 nn_reallocmsg(p, -1000) causes an access violation on Windows
    The test tried to use a fairly large negative number, which
    evaluates to a positive big number on some platforms.  These
    platforms may do various bad things with this result, including
    throw assertions, etc.
    
    Using a negative number smaller than the header size, we can
    test for overflow of the unsigned size_t, which is what the test
    is about.

--- tests/zerocopy.c
+++ tests/zerocopy.c
@@ -40,7 +40,7 @@ void test_allocmsg_reqrep ()
     /*  Try to create an oversized message. */
     p = nn_allocmsg (-1, 0);
     nn_assert (!p && nn_errno () == ENOMEM);
-    p = nn_allocmsg (-1000, 0);
+    p = nn_allocmsg (-3, 0);
     nn_assert (!p && nn_errno () == ENOMEM);
 
     /*  Try to create a message of unknown type. */
@@ -98,7 +98,7 @@ void test_reallocmsg_reqrep ()
     /*  Create message, make sure we handle overflow. */
     p = nn_allocmsg (100, 0);
     nn_assert (p);
-    p2 = nn_reallocmsg (p, -1000);
+    p2 = nn_reallocmsg (p, (size_t)-3);
     errno_assert (nn_errno () == ENOMEM);
     nn_assert (p2 == NULL);
 

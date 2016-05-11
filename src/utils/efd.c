b2599d938ed1ff4523b21475ce09f2ac621e3796
Garrett D'Amore <garrett@damore.org>
2016-05-11 10:44:03 -0700

    attempt to apply timeout fixes for windows.

--- src/utils/efd.c
+++ src/utils/efd.c
@@ -80,7 +80,8 @@ int nn_efd_wait (struct nn_efd *self, int timeout)
             timeout = expire - nn_clock_ms();
             if (timeout < 0) {
                 return -ETIMEDOUT;
-            } else if (timeout > 100) {
+            }
+            if (timeout > 100) {
                 timeout = 100;
             }
         }
@@ -92,7 +93,6 @@ int nn_efd_wait (struct nn_efd *self, int timeout)
             if (expire == 0)
                 return -ETIMEDOUT;
             if ((expire != (uint64_t)-1) && (expire < nn_clock_ms())) {
-                /* NB: will not be true for expire == (uint64_t)-1 */
                 return -ETIMEDOUT;
             }
             continue;
@@ -108,33 +108,66 @@ int nn_efd_wait (struct nn_efd *self, int timeout)
     int rc;
     struct timeval tv;
     SOCKET fd = self->r;
+    uint64_t expire;
 
-    if (nn_slow (fd == INVALID_SOCKET)) {
-        return -EBADF;
-    }
-    FD_SET (fd, &self->fds);
-    if (timeout >= 0) {
+    if (timeout > 0) {
+        expire = nn_clock_ms() + timeout;
         tv.tv_sec = timeout / 1000;
         tv.tv_usec = timeout % 1000 * 1000;
-    }
-    rc = select (0, &self->fds, NULL, NULL, timeout >= 0 ? &tv : NULL);
-
-    if (nn_slow (rc == SOCKET_ERROR)) {
-        rc = nn_err_wsa_to_posix (WSAGetLastError ());
-        errno = rc;
-
-        /*  Treat these as a non-fatal errors, typically occuring when the
-            socket is being closed from a separate thread during a blocking
-            I/O operation. */
-        if (nn_fast (rc == EINTR || rc == ENOTSOCK))
-            return -EINTR;
+    } else {
+        expire = timeout;
     }
 
-    wsa_assert (rc >= 0);
+    for (;;) {
+        if (nn_slow (fd == INVALID_SOCKET)) {
+            return -EBADF;
+        }
+        FD_SET (fd, &self->fds);
+        switch (expire) {
+        case 0:
+            tv.tv_sec = 0;
+            tv.usec = 0;
+            break;
+        case (uint64_t)-1:
+            tv.tv_sec = 0;
+            tv.tv_usec = 100000;
+            break;
+        default:
+            timeout = expire - nn_clock_ms();
+            if (timeout < 0) {
+                return -ETIMEDOUT;
+            }
+            if (timeout > 100) {
+                tv.tv_sec = 0;
+                tv.tv_usec = 100000;
+            } else {
+                tv.tv_sec = timeout / 1000;
+                tv.tv_usec = timeout % 1000 * 1000;
+            }
+        }
+        rc = select (0, &self->fds, NULL, NULL, &tv);
 
-    if (nn_slow (rc == 0))
-        return -ETIMEDOUT;
-    return 0;
+        if (nn_slow (rc == SOCKET_ERROR)) {
+            rc = nn_err_wsa_to_posix (WSAGetLastError ());
+            errno = rc;
+
+            /*  Treat these as a non-fatal errors, typically occuring when the
+                socket is being closed from a separate thread during a blocking
+                I/O operation. */
+            if (rc == EINTR || rc == ENOTSOCK)
+                return -EINTR;
+        } else if (rc == 0) {
+            if (expire == 0)
+                return -ETIMEDOUT;
+            if ((expire != (uint64_t)-1) && (expire < nn_clock_ms())) {
+                return -ETIMEDOUT;
+            }
+            continue;
+	}
+
+        wsa_assert (rc >= 0);
+        return 0;
+    }
 }
 
 #else

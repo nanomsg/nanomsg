0c1aa2b288f6b167dbafe7e29c20e6fc7e71c000
Garrett D'Amore <garrett@damore.org>
2017-11-06 16:06:34 -0800

    Version 1.1.2.  Fix compile error on Linux.

--- src/utils/strncasecmp.h
+++ src/utils/strncasecmp.h
@@ -23,6 +23,8 @@
 #ifndef NN_STRNCASECMP_INCLUDED
 #define NN_STRNCASECMP_INCLUDED
 
+#include <stdlib.h>
+
 extern int nn_strncasecmp(const char *, const char *, size_t);
 
 #endif /* NN_STRNCASECMP_INCLUDED */
